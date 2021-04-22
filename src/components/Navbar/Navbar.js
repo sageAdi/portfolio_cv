dcf256d06a1b9e0de4849e754a8cd5f5c585a40c
sageAdi <adityamaurya100@gmail.com>
2021-04-22 21:55:12 +0530

    Navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import logo from "../../logo.svg";
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#logo">
+            <img src={logo} alt="logo" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#about">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#skills">
+                  Skills
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#project">
+                  Project
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
