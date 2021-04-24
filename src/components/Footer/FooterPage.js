a5db31831d4d90d2c7ac5f6042bba7077bb2711d
sageAdi <adityamaurya100@gmail.com>
2021-04-24 16:15:57 +0530

    added gh pages

--- src/components/Footer/FooterPage.js
+++ src/components/Footer/FooterPage.js
@@ -11,15 +11,15 @@ const FooterPage = () => {
   return (
     <div className="footer">
       <div className="container h-100">
-        <div className="container-fluid h-100 row align-items-center">
+        <div className="container-fluid h-100 row align-items-center m-0 p-0">
           <div className="col-9 p-0 m-0">
             <p className="p-0 m-0">&#169; Copyright 2021 | Aditya Maurya</p>
           </div>
-          <div className="col-3 social-media">
-            <FontAwesomeIcon icon={faFacebookF} />
-            <FontAwesomeIcon icon={faInstagram} />
-            <FontAwesomeIcon icon={faGithub} />
-            <FontAwesomeIcon icon={faGooglePlus} />
+          <div className="col-3 social-media p-0 m-0">
+            <FontAwesomeIcon icon={faFacebookF} size="lg" className="icon" />
+            <FontAwesomeIcon icon={faInstagram} size="lg" className="icon" />
+            <FontAwesomeIcon icon={faGithub} size="lg" className="icon" />
+            <FontAwesomeIcon icon={faGooglePlus} size="lg" className="icon" />
           </div>
         </div>
       </div>
